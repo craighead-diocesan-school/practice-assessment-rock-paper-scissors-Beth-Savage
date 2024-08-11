2891358a8bc78cc6ecf01ae6d4e699ebd0d23334
Beth-Savage <savagebe49@craighead.school.nz>
2024-08-12 09:31:44 +1200

    Added in a stop function
    Added in code so that anytime in the code the user can input stop to finish the game and the final score will be outputted.
    Tested with
    stop
    Stop
    STOP
    All work

--- script.js
+++ script.js
@@ -2,23 +2,25 @@ function paperScissorsRock() {
   //Gives options for the computer to use and outputs the purpose of the code
   let answerOptions = ["rock", "paper", "scissors"]
   alert("We are going to play paper scissors rock. We will do best out of 3")
-  //Creates the variables and there values before they come up and changed in later code
+  //Creates the variables and there values before they come up and are changed in later code
   let roundNumber = 1
   let computersChoice = ""
   let usersChoice = ""
   let computerScore = 0
   let usersScore = 0
   //The while will make the code repeat until all rounds are over
-  while (roundNumber <= 3) {
-    //The computer picks a random output from the array above
+  while (roundNumber <= 3 && usersChoice != "stop") {
+    //Gets the computer to pick a random output from the array above
     let index = Math.floor(Math.random() * 3)
     computersChoice = answerOptions[index]
-    //Askes the user what they want to play and turnsi it into a lower case and extracts any spaces before or after the input to prevent errors in the code
+    //Askes the user what they want to play and turns it fully lower case and extracts any spaces before or after the input to help prevent errors in the code
     usersChoice = prompt("Input your choice of paper, scissors or rock")
     usersChoice = usersChoice.toLowerCase()
     usersChoice = usersChoice.trim()
     //Simply outputs the users and computers choices
-    alert("I chose: " + computersChoice + " You chose: " + usersChoice)
+    if (usersChoice != "stop") {
+      alert("I chose: " + computersChoice + " You chose: " + usersChoice)
+    }
     //Compares the users and computers input and outputs a message accordingly then adds a number to the score and the round counter. If there is a tie or something goes wrong then there will be nothing added to the round counter or the scores
     if (computersChoice == usersChoice) {
       alert("It was a tie, we will try again")
@@ -30,8 +32,8 @@ function paperScissorsRock() {
       alert("You lost this round")
       roundNumber = roundNumber + 1
       computerScore = computerScore + 1
-    } else {
-      alert("Something went wrong please try again")
+    } else if (usersChoice != "stop") {
+      alert("Something went wrong please try again or input 'stop' to finish the game")
     }
   }
   //Compares the scores and outputs who won with a small message
@@ -39,5 +41,7 @@ function paperScissorsRock() {
     alert("I won, good game")
   } else if (computerScore < usersScore) {
     alert("You won, play again?")
+  } else if (computerScore == usersScore) {
+    alert("It ended in a tie")
   }
 }
